3467e03d5e5f7b0b85a54fadd9c590500df26b1d
Student <league@iMac-24.local>
2024-05-23 18:31:11 -0700

    :b:

--- src/main/java/com/techelevator/QuizMakerController.java
+++ src/main/java/com/techelevator/QuizMakerController.java
@@ -2,6 +2,9 @@ package com.techelevator;
 
 import com.techelevator.dao.QuizQuestionDao;
 import com.techelevator.model.QuizQuestion;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -9,7 +12,8 @@ public class QuizMakerController {
 
     private QuizQuestionDao dao;
     private QuizMakerView view;
-
+    public WebClient webClient;
+    public String[] quizzes;
     public QuizMakerController(QuizQuestionDao loader) {
         this.dao = loader;
         this.view = new QuizMakerView();
@@ -20,6 +24,16 @@ public class QuizMakerController {
         /*
          * 1. Get all quizzes
          */
+        webClient = WebClient
+                .builder()
+                .baseUrl("https://opentdb.com/api.php?amount=10")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        quizzes = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam())
+
 
 
         /*
